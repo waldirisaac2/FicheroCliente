65790a2424ef4ecb685dffa1a2bfe42a31736548
waldirisaac2 <waldir.quillamamani@upeu.edu.pe>
2020-08-03 15:46:45 -0500

    primero

---
+++ src/ficherocliente/FICHEROCLIENTE.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ficherocliente;
+
+
+ 
+public class FICHEROCLIENTE{
+public static void main (String[] args){
+    PANEL f = new PANEL();
+    f.setVisible (true);
+ }
+}
